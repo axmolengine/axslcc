4643c3a9cd8f1cafc0286176b57ee316ba855c33
halx99 <halx99@live.com>
2026-07-08 18:29:34 +0800

    Bump version to 3.99.0

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define AXSLCC_MAJOR 3
-#define AXSLCC_MINOR 9
+#define AXSLCC_MINOR 99
 #define AXSLCC_PATCH 0
 
-#define AXSLCC_VERSION_STRING "3.9.0"
+#define AXSLCC_VERSION_STRING "3.99.0"
